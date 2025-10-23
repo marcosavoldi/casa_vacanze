0f41eedcb518e777317ba18e97e443d31128f524
marcosavoldi <marco.savo.92@gmail.com>
2025-10-23 13:19:07 +0200

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyCjorv7BM7krRsqeoozt1mYuXJjVmW3LUk",
+    authDomain: "casa-vacanze-fd31e.firebaseapp.com",
+    projectId: "casa-vacanze-fd31e",
+    storageBucket: "casa-vacanze-fd31e.firebasestorage.app",
+    messagingSenderId: "442503876925",
+    appId: "1:442503876925:web:9a514b66dc97d16d49b554",
+    measurementId: "G-FZ5FVXX7FN"
+};
